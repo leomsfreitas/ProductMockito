5047fcf65da49c14d621b593fe9b318d8c9f38c1
Leo Freitas <leomsfreitas@gmail.com>
2026-03-10 23:52:31 -0300

    feat: invalid test cases

--- src/test/java/UpdateProductTest.java
+++ src/test/java/UpdateProductTest.java
@@ -1,28 +1,31 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class UpdateProductTest {
+    private Product product;
+
     @Mock
     ProductRepository productRepository;
 
     @InjectMocks
     UpdateProductImp updateProductImp;
 
+    @BeforeEach
+    void setUp() {
+        product = new Product(1, "Macbook Air", 1000, 5);
+    }
+
     @Test
     @DisplayName("Should update products successfully using Mockito")
     public void shouldUpdateProductSuccessfully() {
-
-        final Product product = new Product(1, "Macbook Air", 1000, 5);
         when(productRepository.findById(1))
                 .thenReturn(Optional.of(product));
 
@@ -41,5 +44,57 @@ public class UpdateProductTest {
 
         verify(productRepository).save(product);
 
+    }
+
+    @Test
+    @DisplayName("Should throw when price is zero (boundary value)")
+    void shouldFailWhenPriceIsZero() {
+        when(productRepository.findById(1))
+                .thenReturn(Optional.of(product));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() ->
+                updateProductImp.updateProduct(
+                        new ProductDTO(1,"Macbook Neo",0,5)
+                ));
+    }
+
+    @Test
+    @DisplayName("Should throw when price is negative.")
+    void shouldFailWhenPriceIsNegative() {
+        when(productRepository.findById(1))
+                .thenReturn(Optional.of(product));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() ->
+                        updateProductImp.updateProduct(
+                                new ProductDTO(1, "Macbook Neo", -1, 5)
+                        ));
+    }
+
+    @Test
+    @DisplayName("Should throw when quantity is zero.")
+    void shouldFailWhenQuantityIsZero() {
+        when(productRepository.findById(1))
+                .thenReturn(Optional.of(product));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() ->
+                        updateProductImp.updateProduct(
+                                new ProductDTO(1, "Macbook Neo", 10, 0)
+                        ));
+    }
+
+    @Test
+    @DisplayName("Should throw when product does not exists.")
+    void shouldFailWhenProductDoesNotExist() {
+        when(productRepository.findById(99))
+                .thenReturn(Optional.empty());
+
+        assertThatRuntimeException()
+                .isThrownBy(() ->
+                        updateProductImp.updateProduct(
+                                new ProductDTO(99, "Macbook Neo", 10, 5)
+                        ));
     }
 }
